354def9f1304f69093c18ddb296ba0f1bd94332d
Geo Stokes <geo@slashpine.tech>
2022-06-27 14:28:19 -0400

    wip: tweaks to build

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const rimraf = require('rimraf');
+const rename = require('gulp-rename');
 
 // source and destination paths
 const docsSrc = './docs/**/*.html';
@@ -14,6 +15,13 @@ const cssDest = './_docs/css';
 const cssDist = './dist/css'
 const assetsSrc = './assets/**/*';
 const assetsDest = './_docs/assets/';
+const componentScssSrc = './scss/components/*.scss';
+const coreScssSrc = './scss/canopy-ui.scss';
+
+const scssPlugins = [
+  autoprefixer(),
+  cssnano(),
+];
 
 /**
  * Reloads the running browser sync instance to reflect new changes.
@@ -24,18 +32,19 @@ function reloadTask(callback) {
   callback();
 }
 
-/**
- * Builds CSS from SCSS, minify it and auto-prefix.
- */
-function scssTask() {
-  const plugins = [
-    autoprefixer(),
-    cssnano(),
-  ];
-
-  return src(scssSrc, { sourcemaps: true })
+function componentsScssTask() {
+  return src(componentScssSrc, { sourcemaps: true })
     .pipe(sass())
-    .pipe(postcss(plugins))
+    .pipe(postcss(scssPlugins))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(`${cssDest}/components/`, { sourcemaps: '.' }));
+}
+
+function coreScssTask() {
+  return src(coreScssSrc, { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss(scssPlugins))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(dest(cssDest, { sourcemaps: '.' }));
 }
 
@@ -52,7 +61,7 @@ function serveTask(callback) {
   });
 
   watch(docsSrc, series(docsTask));
-  watch(scssSrc, series(scssTask));
+  watch(scssSrc, series(coreScssTask, componentsScssTask));
   watch(`${docsDest}**/*`, series(reloadTask));
   callback();
 }
@@ -81,4 +90,4 @@ function cleanTask(callback) {
   rimraf(docsDest, callback);
 }
 
-exports.default = series(cleanTask, docsTask, assetsTask, scssTask, serveTask);
+exports.default = series(cleanTask, docsTask, assetsTask, coreScssTask, componentsScssTask, serveTask);
